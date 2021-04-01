454c3b9cacbe083e679935b71fd5201a398a4d99
MounikaKolisetty <mouni.kolisetty@gmail.com>
2021-04-01 17:53:27 +0530

    Accuracy of about 90%

--- Project/main.py
+++ Project/main.py
@@ -11,6 +11,10 @@
 
 import os
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix
 path = r'/Users/mouni.kolisetty/Documents/Sem 3/machine learning/Files for task 5'
 
 # for subdir, dirs, files in os.walk(path):
@@ -22,7 +26,8 @@ path = r'/Users/mouni.kolisetty/Documents/Sem 3/machine learning/Files for task 
 object1_data = pd.DataFrame()
 object2_data = pd.DataFrame()
 object3_data = pd.DataFrame()  
-test_data = pd.DataFrame()    
+training_set = pd.DataFrame()  
+validation_set = pd.DataFrame()  
 for subdir, dirs, files in os.walk(path):
     for dir in dirs:
         subdir_path = os.path.join(subdir, dir)
@@ -32,8 +37,9 @@ for subdir, dirs, files in os.walk(path):
                         df = pd.read_excel(os.path.join(subdir1,f), header = None, usecols = "G:DZZ")
                         df.insert(0, "Object Type", "Object 1")
                         object1_data = object1_data.append(df,ignore_index=True)
-                print("object1_data is uploaded")
-                print(object1_data)
+                training_set1, validation_set1 = train_test_split(object1_data, test_size = 0.2, random_state = 21)
+                print(training_set1)
+                print(validation_set1)
                 print(object1_data.isnull().sum().sum()) #To check is there is any null(NaN) in the data
                 
             elif dir == "Object 2":
@@ -41,21 +47,54 @@ for subdir, dirs, files in os.walk(path):
                         df = pd.read_excel(os.path.join(subdir1,f), header = None, usecols = "G:DZZ")
                         df.insert(0, "Object Type", "Object 2")
                         object2_data = object2_data.append(df,ignore_index=True)
-                print("object2_data is uploaded")
-                print(object2_data)
+                training_set2, validation_set2 = train_test_split(object2_data, test_size = 0.2, random_state = 21)
+                print(training_set2)
+                print(validation_set2)
                 print(object2_data.isnull().sum().sum())
             elif dir == "Object 3":
                 for f in files1:
                         df = pd.read_excel(os.path.join(subdir1,f), header = None, usecols = "G:DZZ")
                         df.insert(0, "Object Type", "Object 3")
                         object3_data = object3_data.append(df,ignore_index=True)
-                print("object3_data is uploaded")
-                print(object3_data)
+                training_set3, validation_set3 = train_test_split(object3_data, test_size = 0.2, random_state = 21)
+                print(training_set3)
+                print(validation_set3)
                 print(object3_data.isnull().sum().sum())
-test_data = test_data.append(object1_data,ignore_index = True)
-test_data = test_data.append(object2_data,ignore_index = True)
-test_data = test_data.append(object3_data,ignore_index = True)
-print(test_data)
+training_set = training_set.append(training_set1,ignore_index = True)
+training_set = training_set.append(training_set2,ignore_index = True)
+training_set = training_set.append(training_set3,ignore_index = True)
+print(training_set)
+
+validation_set = validation_set.append(validation_set1,ignore_index = True)
+validation_set = validation_set.append(validation_set2,ignore_index = True)
+validation_set = validation_set.append(validation_set3,ignore_index = True)
+print(validation_set)
+
+X_train = training_set.iloc[:, 1:-1].values
+Y_train = training_set.iloc[:, 0].values
+X_val = validation_set.iloc[:, 1:-1].values
+y_val = validation_set.iloc[:, 0].values
+
+def accuracy(confusion_matrix):
+   diagonal_sum = confusion_matrix.trace()
+   sum_of_all_elements = confusion_matrix.sum()
+   return diagonal_sum / sum_of_all_elements
+
+#Initializing the MLPClassifier
+classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
+
+#Fitting the training data to the network
+classifier.fit(X_train, Y_train)
+
+#Predicting y for X_val
+y_pred = classifier.predict(X_val)
+
+#Comparing the predictions against the actual observations in y_val
+cm = confusion_matrix(y_pred, y_val)
+
+#Printing the accuracy
+print(cm)
+print("Accuracy of MLPClassifier : ", accuracy(cm))
 print("Completed")
             
             
