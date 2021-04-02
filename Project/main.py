81670d5372d67b71d90803fd339f404f453689f6
MounikaKolisetty <mouni.kolisetty@gmail.com>
2021-04-02 19:22:09 +0530

    Accuracy of 92% along with all the measures except ROC

--- Project/main.py
+++ Project/main.py
@@ -1,28 +1,17 @@
-# import os
-# import pandas as pd
-# from os import walk
-
-# path = r'/Users/mouni.kolisetty/Documents/Sem 3/machine learning/Files for task 5'
-# for f in os.listdir(path):
-#    if f.startswith("Object"):
-#    print (os.path.join(path , f))
-# # df = pd.read_excel (r'/Users/mouni.kolisetty/Documents/Sem 3/machine learning/Files for task 5/Object 1/record_data_2020-10-29_10-41-1_H.xlsx')
-# # print (df)
-
 import os
+import re
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import plot_confusion_matrix
+import matplotlib.pyplot as plt
+
 path = r'/Users/mouni.kolisetty/Documents/Sem 3/machine learning/Files for task 5'
 
-# for subdir, dirs, files in os.walk(path):
-#     for file in files:
-#         if file.endswith(".xlsx"):
-#             df = pd.read_excel (os.path.join(subdir, file))
-#             print(df)
-#             #print(os.path.join(subdir, file))
 object1_data = pd.DataFrame()
 object2_data = pd.DataFrame()
 object3_data = pd.DataFrame()  
@@ -38,9 +27,6 @@ for subdir, dirs, files in os.walk(path):
                         df.insert(0, "Object Type", "Object 1")
                         object1_data = object1_data.append(df,ignore_index=True)
                 training_set1, validation_set1 = train_test_split(object1_data, test_size = 0.2, random_state = 21)
-                print(training_set1)
-                print(validation_set1)
-                print(object1_data.isnull().sum().sum()) #To check is there is any null(NaN) in the data
                 
             elif dir == "Object 2":
                 for f in files1:
@@ -48,40 +34,32 @@ for subdir, dirs, files in os.walk(path):
                         df.insert(0, "Object Type", "Object 2")
                         object2_data = object2_data.append(df,ignore_index=True)
                 training_set2, validation_set2 = train_test_split(object2_data, test_size = 0.2, random_state = 21)
-                print(training_set2)
-                print(validation_set2)
-                print(object2_data.isnull().sum().sum())
+               
             elif dir == "Object 3":
                 for f in files1:
                         df = pd.read_excel(os.path.join(subdir1,f), header = None, usecols = "G:DZZ")
                         df.insert(0, "Object Type", "Object 3")
                         object3_data = object3_data.append(df,ignore_index=True)
                 training_set3, validation_set3 = train_test_split(object3_data, test_size = 0.2, random_state = 21)
-                print(training_set3)
-                print(validation_set3)
-                print(object3_data.isnull().sum().sum())
+
+#Appending training data from all the object types        
 training_set = training_set.append(training_set1,ignore_index = True)
 training_set = training_set.append(training_set2,ignore_index = True)
 training_set = training_set.append(training_set3,ignore_index = True)
-print(training_set)
 
+#Appending validation data from all the object types  
 validation_set = validation_set.append(validation_set1,ignore_index = True)
 validation_set = validation_set.append(validation_set2,ignore_index = True)
 validation_set = validation_set.append(validation_set3,ignore_index = True)
-print(validation_set)
+
 
 X_train = training_set.iloc[:, 1:-1].values
 Y_train = training_set.iloc[:, 0].values
 X_val = validation_set.iloc[:, 1:-1].values
 y_val = validation_set.iloc[:, 0].values
 
-def accuracy(confusion_matrix):
-   diagonal_sum = confusion_matrix.trace()
-   sum_of_all_elements = confusion_matrix.sum()
-   return diagonal_sum / sum_of_all_elements
-
 #Initializing the MLPClassifier
-classifier = MLPClassifier(hidden_layer_sizes=(150,100,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
+classifier = MLPClassifier(hidden_layer_sizes=(100,50,50), max_iter=300,activation = 'relu',solver='adam',random_state=1)
 
 #Fitting the training data to the network
 classifier.fit(X_train, Y_train)
@@ -91,12 +69,46 @@ y_pred = classifier.predict(X_val)
 
 #Comparing the predictions against the actual observations in y_val
 cm = confusion_matrix(y_pred, y_val)
+print(cm, "\n")
+
+#Ploting confusion matrix
+disp = plot_confusion_matrix(classifier, X_val, y_val,
+                                 cmap=plt.cm.Blues)
+plt.show()
 
 #Printing the accuracy
-print(cm)
-print("Accuracy of MLPClassifier : ", accuracy(cm))
-print("Completed")
-            
-            
-            
-          
+accuracy = accuracy_score(y_pred, y_val)
+print("Accuracy of MLPClassifier : ", accuracy, "\n")
+
+#Printing F1
+print(classification_report(y_pred,y_val), "\n")
+
+FP = cm.sum(axis=0) - np.diag(cm) 
+FN = cm.sum(axis=1) - np.diag(cm)
+TP = np.diag(cm)
+TN = cm.sum() - (FP + FN + TP)
+FP = FP.astype(float)
+FN = FN.astype(float)
+TP = TP.astype(float)
+TN = TN.astype(float)
+
+print("TruePostive(TP) : ", TP, "\n")
+print("TrueNegative(TN) : ", TN, "\n")
+print("FalsePostive(FP) : ", FP, "\n")
+print("False Negative(FN) : ", FN, "\n")
+
+# False discovery rate
+FDR = FP/(TP+FP)
+print("False Discovery Rate(FDR) : ", FDR, "\n")
+
+# Negative predictive value
+NPV = TN/(TN+FN)
+print("Negative Preductive Value(NPV) : ", NPV, "\n")
+
+# Sensitivity, hit rate, recall, or true positive rate
+TPR = TP/(TP+FN)
+print("True Positive Rate(TPR) : ", TPR, "\n")
+
+# Specificity, selectivity or true negative rate
+TNR = TN/(TN+FP) 
+print("True Negative Rate(TNR) : ", TNR, "\n")
